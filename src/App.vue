3bca6379818d7e13cf83b6e4aa46b0baeaed39ad
Mutohir <mutohirokuniyi@gmail.com>
2024-02-07 17:21:15 +0100

    feat: used scss to generate utility classes

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,28 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 class=" fs-700 text-clr-neutral-100 bg-clr-accent-500">statinhg yhe padfs</h1>
+  <FontAwesomeIcon class="envelope" :icon="faEnvelope" />
+<div class="flex">
+  
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+    <p>hello</p>
+</div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+h1{
+  /* font-family: var(--ff-accent); */
+  /* font-size: var(--fs-800); */
+  /* color: var(--clr-primary-400); */
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.envelope {
+  color: var(--clr-primary-900);
 }
 </style>
