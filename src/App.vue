5b1bc47a36050820a5a8e9d13b587fdfcf37e6ca
Mutohir <mutohirokuniyi@gmail.com>
2024-02-09 11:57:50 +0100

    feat: add input component class

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 </script>
 
 <template>
@@ -8,13 +9,24 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
     <h1 class="text-center breakout fw-semibold ff-base fs-700 text-clr-accent-500 bg-clr-secondary-500">statinhg yhe padfs</h1>
   </div>
   <FontAwesomeIcon class="envelope" :icon="faEnvelope" />
+  <FontAwesomeIcon class="house" :icon="faHouse" />
 <div class="flex gap-space-s">
   
     <p>hello</p>
     <p>hello</p>
     <p>hello</p>
     <p>hello</p>
-</div>
+
+  </div>
+  <div class="flex items-center gap-space-m">
+    <button class="button" data-type="primary">Primary</button>
+    <button class="button" data-type="secondary">Secondary</button>
+    <button class="button" data-type="tertiary">Tertiary</button>
+    <button class="button" disabled data-type="primary">P dis</button>
+    <button class="button" disabled data-type="secondary">sec dis</button>
+  </div>
+
+  <input class="input" type="text" placeholder="this is an input">
 </template>
 
 <style scoped>
