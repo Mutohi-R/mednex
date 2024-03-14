3435e88f795b3657fe930d710cabde9230624035
Mutohir <mutohirokuniyi@gmail.com>
2024-03-14 17:12:49 +0100

    mod: changed footer UI and made it responsive

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
-  <footer class="footer | text-clr-neutral-100">
-    <div class="content-grid | bg-clr-primary-600">
-      <section
+  <footer class="footer | text-clr-neutral-100 fs-300">
+    <div class="content-grid | bg-clr-primary-900">
+      <!-- <section
         class="primary-footer | breakout | grid mx-auto justify-between gap-16 items-center"
       >
         <div class="logo">
@@ -70,16 +70,63 @@
           <p class="fs-300 text-clr-primary-600">Language</p>
         </div>
         <div class="footer__icons | flex items-center mx-auto gap-6">
-          <!-- twitter -->
           <Twitter class="cursor-pointer" />
-          <!-- facebook -->
           <Facebook class="cursor-pointer" />
-          <!-- linkedIn -->
           <LinkedIn class="cursor-pointer" />
-          <!-- youtube -->
           <Youtube class="cursor-pointer" />
-          <!-- instagram -->
           <Instagram class="cursor-pointer" />
+        </div>
+      </section> -->
+      <section class="primary-footer | breakout">
+        <LogoWhite />
+        <div class="content">
+          <div class="solutions ~ flow">
+            <h3>Solutions</h3>
+            <p>Hospital Search</p>
+            <p>Healthcare</p>
+            <p>Hospital Share</p>
+          </div>
+          <div class="use-cases | flow">
+            <h3>Use Cases</h3>
+            <p>Mednex for Hospitals</p>
+            <p>Mednex for Clinics</p>
+            <p>MedNex for Providers</p>
+            <p>MedNex for Patients</p>
+          </div>
+          <div class="resources | flow">
+            <h3>Resources</h3>
+            <p>Blog</p>
+            <p>Knowledge Base</p>
+            <p>Developers API</p>
+          </div>
+          <div class="company | flow ">
+            <h3>Company</h3>
+            <p>About Us</p>
+            <p>Help Center</p>
+            <p>Security</p>
+            <p>Events</p>
+            <p>Hospitals</p>
+            <p>Contact Us</p>
+            <p>Privacy Policy</p>
+          </div>
+          <div class="subscribe | flow">
+            <h3>Subscribe</h3>
+            <form>
+              <input class="" type="email" placeholder="Email Address" />
+              <button class="" data-type="">Subscribe</button>
+            </form>
+          </div>
+        </div>
+      </section>
+
+      <section class="secondary-footer | full-width | bg-clr-secondary-500 ">
+        <div>
+          <p class="text-clr-primary-600">&copy; 2024 MedNex. All rights reserved</p>
+          <div class="social-icons">
+            <FontAwesomeIcon class="icon" :icon="faTwitter" />
+            <FontAwesomeIcon class="icon" :icon="faInstagram" />
+            <FontAwesomeIcon class="icon" :icon="faLinkedinIn" />
+          </div>
         </div>
       </section>
     </div>
@@ -87,29 +134,101 @@
 </template>
 
 <script setup lang="ts">
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faTwitter, faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import LogoWhite from "@/components/LogoWhite.vue";
 </script>
 
 <style scoped>
 .primary-footer {
+  display: grid;
+  gap: 1rem;
   padding-block: 3.375rem;
   flex-direction: column;
+
+  .content {
+    display: grid;
+    grid-template-areas:
+      "solutions use-cases resources company subscribe";
+    gap: 1.5rem;
+
+    @media screen and (max-width: 520px) {
+      grid-template-areas: 
+      "solutions use-cases" 
+      "resources company" 
+      "subscribe subscribe";
+    }
+
+    h3 {
+      color: var(--clr-neutral-100);
+      text-transform: uppercase;
+      opacity: .6;
+    }
+  }
+
+  .solutions {
+    grid-area: solutions;
+  }
+
+  .use-cases {
+    grid-area: use-cases;
+  }
+
+  .resources {
+    grid-area: resources;
+  }
+
+  .company {
+    grid-area: company;
+  }
+
+  .subscribe {
+    grid-area: subscribe;
+
+    form {
+      display: grid;
+      gap: 0.75rem;
+
+      input {
+        padding: 0.5rem;
+        background: var(--clr-neutral-100);
+        border-radius: 0.25rem;
+      }
+      
+      button {
+        padding: 0.5rem;
+        background: var(--clr-accent-300);
+        border-radius: 0.25rem;
+      }
+    }
+  }
 }
 
 .secondary-footer {
   padding-block: 0.875rem;
-  /* grid-auto-flow: column; */
-  /* place-items: center; */
+
+  > div {
+    display: flex;
+    justify-content: space-between;
+
+    .social-icons {
+      display: flex;
+      gap: var(--space-2xs);
+
+      .icon {
+        color: var(--clr-primary-600);
+        font-size: 1.3rem;
+
+        &:hover {
+          color: var(--clr-accent-300);
+          cursor: pointer;
+        }
+      }
+    }
+  }
 }
 
-.extra {
-  display: grid;
-  grid-template-areas:
-    "product support"
-    "company resources";
-}
-
-@media screen and (min-width: 980px) {
+/* @media screen and (max-width:520px) {
   .primary-footer {
     grid-auto-flow: column;
     margin: 0;
@@ -117,21 +236,6 @@ import LogoWhite from "@/components/LogoWhite.vue";
   .extra {
     grid-template-areas: "product support company resources";
   }
-}
+} */
 
-.product {
-  grid-area: product;
-}
-
-.support {
-  grid-area: support;
-}
-
-.company {
-  grid-area: company;
-}
-
-.resources {
-  grid-area: resources;
-}
 </style>
