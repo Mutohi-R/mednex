0499d8f8ceef2e4dd842071a36371d08f429ae9c
Mutohir <mutohirokuniyi@gmail.com>
2024-03-15 14:59:34 +0100

    feat: add test for landing and hospital page

--- src/test/LandingPage.spec.ts
+++ src/test/LandingPage.spec.ts
@@ -1,17 +1,17 @@
 import { describe, it, expect, beforeEach } from "vitest";
 import { mount } from "@vue/test-utils";
 import { render, screen } from "@testing-library/vue";
-import Hospitals from "@/pages/Hospitals.vue";
+import LandingPage from "@/pages/LandingPage.vue";
 import { createPinia, setActivePinia } from "pinia";
 
-describe("Hospitals page test suite", () => {
+describe("Landing page test suite", () => {
 
     beforeEach(() => {
         setActivePinia(createPinia())
     })
 
     it("should render correctly", async () => {
-        const wrapper = mount(Hospitals);
-        expect(wrapper.text()).toContain("Discover healthcare near you")
+        const wrapper = mount(LandingPage);
+        expect(wrapper.text()).toContain("Your Personalized Path To Better Health Starts Here")
     });
 });
